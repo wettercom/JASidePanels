110b0409b0cfbf68830c41e359cf91a7b22b8dd0
StuFF mc <mc@stuffmc.com>
2013-03-21 14:52:29 +0100

    WET specific check code

--- JASidePanels/Source/JASidePanelSegueToCenter.m
+++ JASidePanels/Source/JASidePanelSegueToCenter.m
@@ -15,9 +15,14 @@
     UIViewController *dst = (UIViewController *) self.destinationViewController;
     JASidePanelController *sidePC = src.sidePanelController;
     Class navigationController = [UINavigationController class];
-    if (![dst isKindOfClass:navigationController] && [sidePC.centerPanel isKindOfClass:navigationController]) {
-        UINavigationController *navigationController = (UINavigationController*)sidePC.centerPanel;
-        [navigationController pushViewController:dst animated:YES];
+    if (![dst isKindOfClass:navigationController] && ([sidePC.centerPanel isKindOfClass:navigationController] || [sidePC.centerPanel isKindOfClass:[WETRootViewController class]])) {
+        UINavigationController *centerNavigationController;
+        if ([sidePC.centerPanel isKindOfClass:navigationController]) {
+            centerNavigationController = (UINavigationController*)sidePC.centerPanel;
+        } else {
+            centerNavigationController = ((WETRootViewController*)sidePC.centerPanel).dataViewController;
+        }
+        [centerNavigationController pushViewController:dst animated:YES];
         [sidePC showCenterPanelAnimated:YES];
     } else {
         [sidePC setCenterPanel:dst];
